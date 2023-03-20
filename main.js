3dc7b763b7d4814f300bc3f9682f17200353f913
Vincent Jacquemin <vincent@impulsenow.be>
2023-03-20 03:31:28 +0100

    added comments for clarification

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ import Spaceship from './scripts/spaceship.js';
 
 const xWing = new Spaceship(
   'https://assets.website-files.com/5cfa0a3c809181819a5fd8c2/607db2672488b89e92bba479_XWing.svg',
-  150,
+  450,
   150,
   false
 );
 
+//select board
 const board = document.getElementById('board');
+//create box for xwings and sets initial position
 const xWingsBox = document.createElement('img');
 xWingsBox.setAttribute('src', xWing.url);
 xWingsBox.setAttribute('class', 'spaceShip');
 xWingsBox.style.left = xWing.x + 'px';
 board.appendChild(xWingsBox);
 
+// refresh xWing position
 const moveXWing = () => {
   xWingsBox.style.left = xWing.x + 'px';
   console.log(xWing.x);
 };
 
+// set commands
 addEventListener('keydown', (e) => {
   if (e.key == 'ArrowRight' && xWing.x < 900) {
     xWing.x += 50;
--- scripts/spaceship.js
+++ scripts/spaceship.js
@@ -6,3 +6,7 @@ export default class Spaceship {
     this.hit = hit;
   }
 }
+//url is img URL (spaceship sprite)
+// x is lateral position
+// y is currently not used
+// hit shoud triggers gameover - currently not used
